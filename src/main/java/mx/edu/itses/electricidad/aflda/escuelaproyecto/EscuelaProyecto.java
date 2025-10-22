351bf521fd88620db2f942d50637d811cc7bda9c
f3rn4nd0l0 <l.loaa050209@itses.edu.mx>
2025-10-22 08:26:22 -0600

    Inicio de proyecto

---
+++ src/main/java/mx/edu/itses/electricidad/aflda/escuelaproyecto/EscuelaProyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.edu.itses.electricidad.aflda.escuelaproyecto;
+
+/**
+ *
+ * @author ferlo
+ */
+public class EscuelaProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
