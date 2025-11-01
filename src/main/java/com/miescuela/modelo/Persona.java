76843449093ac1a6efb9369a968bd1bc367269f8
erick240603 <l.yabe060324@itses.edu.mx>
2025-11-01 10:23:08 -0600

    Clases abstractas

--- src/main/java/com/miescuela/modelo/Persona.java
+++ src/main/java/com/miescuela/modelo/Persona.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.miescuela.modelo;
 
-/**
- *
- * @author ferlo
- */
 public class Persona {
-    
+
+    private String id;
+    private String nombre;
+    private String apellido;
+
+    public Persona() {
+
+    }
+
+    public Persona(String id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id='" + id + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                '}';
+    }
 }
+
