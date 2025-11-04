a8dd58fe05c9fa7ada7568f79a00c2d430edd67c
Sebas-jimenez-M <l.jims051226@itses.edu.mx>
2025-11-04 08:49:44 -0600

    agregar listas y metodos

--- src/main/java/com/miescuela/ui/VentanaListaMaterias.java
+++ src/main/java/com/miescuela/ui/VentanaListaMaterias.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.miescuela.iu;
+package com.miescuela.ui;
 
 /**
  *
